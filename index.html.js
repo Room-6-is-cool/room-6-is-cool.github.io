b1254190dcb1db1d6349c84b178542afabe88fa4
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-07-23 10:16:10 +1200

    Update index.html.js

--- index.html.js
+++ index.html.js
@@ -1,1 +1,10 @@
 document.body.style.width = window.innerWidth;
+if (!(document.title == "Room 6 is cool!")) {
+	document.title = "Room 6 is cool!";
+	var warningDiv = document.createElement("div"); 
+	var warningP = document.createElement("p");
+	var warningC = document.createTextNode("Orion has probably tampered with this page.");
+	warningP.appendChild(warningC);
+	warningDiv.appendChild(warningP);
+	document.getElementById("mainContent").appendChild(warningDiv);
+}
