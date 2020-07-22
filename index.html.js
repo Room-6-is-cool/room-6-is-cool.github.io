4d8499b942f3b4f1e768a210a6f8a69da9c0949f
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-07-23 10:23:00 +1200

    Update index.html.js

--- index.html.js
+++ index.html.js
@@ -1,5 +1,5 @@
 document.body.style.width = window.innerWidth;
-if (!(document.title == "Room 6 is cool!")) {
+if (!(document.title == "Room 6 is cool!" && document.getElementById("headerText").innerHtml == "Room 6 is Cool")) {
 	document.title = "Room 6 is cool!";
 	var warningDiv = document.createElement("div"); 
 	var warningP = document.createElement("p");
