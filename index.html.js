cd23fbc92acbf6e0ab127e4353e4104fa73b40de
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-07-23 13:50:21 +1200

    Update index.html.js

--- index.html.js
+++ index.html.js
@@ -1,3 +1,7 @@
+// Read the search params
+const params = new URLSearchParams(window.location.search);
+
+// Check if Orion has tampered with the title of the website
 document.body.style.width = window.innerWidth;
 if (!(document.title == "Room 6 is cool!"/* && document.getElementById("headerText").innerHtml == "Room 6 is Cool"*/)) {
 	document.title = "Room 6 is cool!";
@@ -12,15 +16,48 @@ if (!(document.title == "Room 6 is cool!"/* && document.getElementById("headerTe
 // Get mainContent
 var mainContent = document.getElementById("mainContent");
 
-// Make the header
-var header = document.createElement("h2");
-var htn = document.createTextNode("Here's a really good GIF:");
-header.appendChild(htn);
+if (!params.has('page')) {
+	// Make the header
+	var header = document.createElement("h2");
+	var htn = document.createTextNode("Here's a really good GIF:");
+	header.appendChild(htn);
 
-// Make the doge GIF
-var doge = document.createElement("img");
-doge.src = "https://lh3.googleusercontent.com/-HChSxzMu7uE/XxeV2WJUmxI/AAAAAAAABhE/SWKt_vXboi8SAhfCJ1OY5P6CMlTo05ttACK8BGAsYHg/s0/2020-07-21.gif";
+	// Make the doge GIF
+	var doge = document.createElement("img");
+	doge.src = "https://lh3.googleusercontent.com/-HChSxzMu7uE/XxeV2WJUmxI/AAAAAAAABhE/SWKt_vXboi8SAhfCJ1OY5P6CMlTo05ttACK8BGAsYHg/s0/2020-07-21.gif";
 
-// Add these elements to mainContent
-mainContent.appendChild(header);
-mainContent.appendChild(doge);
+	// Add these elements to mainContent
+	mainContent.appendChild(header);
+	mainContent.appendChild(doge);
+}
+
+else {
+	if (params.get('page') == 'index') {
+		// Make the header
+		var header = document.createElement("h2");
+		var htn = document.createTextNode("Here's a really good GIF:");
+		header.appendChild(htn);
+
+		// Make the doge GIF
+		var doge = document.createElement("img");
+		doge.src = "https://lh3.googleusercontent.com/-HChSxzMu7uE/XxeV2WJUmxI/AAAAAAAABhE/SWKt_vXboi8SAhfCJ1OY5P6CMlTo05ttACK8BGAsYHg/s0/2020-07-21.gif";
+
+		// Add these elements to mainContent
+		mainContent.appendChild(header);
+		mainContent.appendChild(doge);
+	}
+	
+	if (params.get('page') == 'links') {
+		var header = document.createElement("h2");
+		var htn = document.createTextNode("Below are some good links:");
+		header.appendChild(htn);
+		
+		var k3dlink = document.createElement('a');
+		k3dlink.href = 'https://kettle3d.github.io/';
+		var ltn = document.createTextNode("Kettle3D - A great game that we're working on");
+		k3dlink.appendChild(ltn);
+		
+		mainContent.appendChild(header);
+		mainContent.appendChild(k3dlink);
+	}
+}
