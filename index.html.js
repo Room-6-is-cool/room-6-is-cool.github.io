2e1a221ed8d56ba8c1bd99d43f2614eca8d95fc4
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-07-23 12:25:38 +1200

    Update index.html.js

--- index.html.js
+++ index.html.js
@@ -8,3 +8,19 @@ if (!(document.title == "Room 6 is cool!"/* && document.getElementById("headerTe
 	warningDiv.appendChild(warningP);
 	document.getElementById("mainContent").insertBefore(warningDiv, document.getElementById("kettle3d"));
 }
+
+// Get mainContent
+var mainContent = document.getElementById("mainContent");
+
+// Make the header
+var header = document.createElement("h2");
+var htn = document.createTextNode("Here's a really good GIF:");
+header.appendChild(htn);
+
+// Make the doge GIF
+var doge = document.createElement("img");
+doge.src = "https://lh3.googleusercontent.com/-HChSxzMu7uE/XxeV2WJUmxI/AAAAAAAABhE/SWKt_vXboi8SAhfCJ1OY5P6CMlTo05ttACK8BGAsYHg/s0/2020-07-21.gif";
+
+// Add these elements to mainContent
+mainContent.appendChild(header);
+mainContent.appendChild(doge);
